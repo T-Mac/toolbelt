e0cc02c5cb4fe19ad48cdf6737046b41908f7cc5
David Dollar <ddollar@gmail.com>
2012-02-16 14:39:10 -0500

    new batch launcher that is x86 and x64 compatible

--- dist/resources/exe/heroku.bat
+++ dist/resources/exe/heroku.bat
@@ -1,6 +1,27 @@
 @ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"%ProgramFiles(x86)%\ruby-1.9.3\\bin\\ruby.exe" "heroku" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
+
+:: determine if this is x86 or x64
+if "%ProgramFiles%" == "%ProgramW6432%" goto x64
+goto x86
+
+:x86
+set HerokuRubyPath=%ProgramFiles%\ruby-1.9.3
+goto launch
+
+:x64
+set HerokuRubyPath=%ProgramFiles(x86)%\ruby-1.9.3
+goto launch
+
+:launch
+
+:: determine if this is an NT operating system
+if not "%~f0" == "~f0" goto WinNT
+goto Win9x
+
+:Win9x
+@"%HerokuRubyPath%\bin\ruby.exe" "heroku" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto :EOF
+
 :WinNT
-@"%ProgramFiles(x86)%\ruby-1.9.3\\bin\\ruby.exe" "%~dpn0" %*
+@"%HerokuRubyPath%\bin\ruby.exe" "%~dpn0" %*
+goto :EOF
